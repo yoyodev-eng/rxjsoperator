fd6cf72899cfb3040d2df8ec7242dbbb96335f84
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-01-06 17:24:58 +0200

    add new title, add zip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,6 +34,7 @@ import { MergeComponent } from './operators2/merge/merge.component';
 import { BufferComponent } from './operators2/buffer/buffer.component';
 import { PartitionComponent } from './operators2/partition/partition.component';
 import { ThrottleComponent } from './operators2/throttle/throttle.component';
+import { ZipComponent } from './operators3/zip/zip.component';
 
 const routes: Routes = [
   { path: 'range', component: RangeComponent },
@@ -70,6 +71,7 @@ const routes: Routes = [
   { path: 'buffer', component: BufferComponent },
   { path: 'partition', component: PartitionComponent },
   { path: 'throttle', component: ThrottleComponent },
+  { path: 'zip', component: ZipComponent },
 ];
 
 @NgModule({
---
+++ src/app/operators3/zip/zip.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs/operators';
+import { of, zip } from 'rxjs';
+
+@Component({
+  selector: 'app-zip',
+  templateUrl: './zip.component.html',
+  styleUrls: ['./zip.component.scss']
+})
+export class ZipComponent implements OnInit {
+  output = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    // After all observables emit, emit values as an array
+
+    const sourceOne = of('Hello');
+    const sourceTwo = of('World!');
+    const sourceThree = of('Goodbye');
+    const sourceFour = of('World!');
+    const example = zip(
+      sourceOne,
+      sourceTwo.pipe(delay(1000)),
+      sourceThree.pipe(delay(2000)),
+      sourceFour.pipe(delay(3000))
+    );
+    const subscribe = example.subscribe(val => this.output.push(val));
+  }
+}
