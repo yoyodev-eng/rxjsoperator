37769943ee0c312dda0cbd75543a8005105f0265
GoldenstarUpwork <juvencocastro@gmail.com>
2019-01-12 00:02:13 +0200

    add let

---
+++ src/app/operators3/let/let.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-let',
+  templateUrl: './let.component.html',
+  styleUrls: ['./let.component.scss']
+})
+export class LetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
