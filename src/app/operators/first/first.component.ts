5d772a50fff0b249ae16c87e190eed426a104923
GoldenstarUpwork <juvencocastro@gmail.com>
2019-01-15 07:04:04 +0200

    components refactoring

--- src/app/operators/first/first.component.ts
+++ src/app/operators/first/first.component.ts
@@ -10,14 +10,13 @@ import { first } from 'rxjs/operators';
 export class FirstComponent implements OnInit {
   output = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     // Emit the first value or first to pass provided expression.
 
     const source = from([1, 2, 3, 4, 5]);
     const example = source.pipe(first());
-    const subscribe = example.subscribe(val => this.output.push(`First value: ${val}`));
+    example.subscribe(val => this.output.push(`First value: ${val}`));
   }
-
 }
--- src/app/operators/from/from.component.ts
+++ src/app/operators/from/from.component.ts
@@ -7,18 +7,16 @@ import { from } from 'rxjs';
   styleUrls: ['./from.component.scss']
 })
 export class FromComponent implements OnInit {
-
   values = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-        // Turn an array, promise, or iterable into an observable.
-        const source = from('Hello world!');
+    // Turn an array, promise, or iterable into an observable.
+    const source = from('Hello world!');
 
-        source.pipe().subscribe(val => {
-          this.values.push(val);
-        });
+    source.pipe().subscribe(val => {
+      this.values.push(val);
+    });
   }
-
 }
