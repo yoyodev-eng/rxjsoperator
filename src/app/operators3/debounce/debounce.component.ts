b3c3fd68f7ea028c58aedb57190a8f5a2644d217
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-01-31 07:03:46 +0200

    formatting for operators3  directory, remove shared

--- src/app/operators3/debounce/debounce.component.ts
+++ src/app/operators3/debounce/debounce.component.ts
@@ -18,16 +18,19 @@ export class DebounceComponent implements OnInit {
     // based on selector function, between output.
 
     const source = fromEvent(document, 'click');
-    const example = source.pipe(map(event => `Click time with debounce: ${event.timeStamp}`));
-    example.pipe(debounce(() => timer(500))).subscribe(val => this.output.push(val));
+    const example = source.pipe(
+      map(event => `Click time with debounce: ${event.timeStamp}`)
+    );
+    example
+      .pipe(debounce(() => timer(500)))
+      .subscribe(val => this.output.push(val));
 
     // DebounceTime - Discard emitted values that take less than the specified time between output
 
     const source2 = fromEvent(document, 'click');
-    const example2 = source2.pipe(map(event => `Click time with debounceTime: ${event.timeStamp}`));
+    const example2 = source2.pipe(
+      map(event => `Click time with debounceTime: ${event.timeStamp}`)
+    );
     example2.pipe(debounceTime(500)).subscribe(val => this.output2.push(val));
-
   }
 }
-
-
