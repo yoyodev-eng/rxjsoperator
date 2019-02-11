f90a9e165d1dd6d00c949f0e982f7f49781f56cb
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-02-11 07:24:58 +0200

    example in sample

--- src/app/operators2/sample/sample.component.ts
+++ src/app/operators2/sample/sample.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
 import { sample } from 'rxjs/operators';
+import { interval, zip, from } from 'rxjs';
 
 @Component({
   selector: 'app-sample',
@@ -9,14 +9,25 @@ import { sample } from 'rxjs/operators';
 })
 export class SampleComponent implements OnInit {
   output = [];
+  output2 = [];
 
   constructor() {}
 
   ngOnInit() {
     // Sample from source when provided observable emits.
+    // Example 1: Sample source every 2 seconds
 
     const source = interval(1000);
     const example = source.pipe(sample(interval(2000)));
     example.subscribe(val => this.output.push(val));
+
+    // Example 2: Sample source when interval emits
+
+    const source2 = zip(
+      from(['Joe', 'Frank', 'Bob']),
+      interval(2000)
+    );
+    const example2 = source2.pipe(sample(interval(2500)));
+    example2.subscribe(val => this.output2.push(val));
   }
 }
